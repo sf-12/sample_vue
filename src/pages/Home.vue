a3f86ae26b1847c19ebd536fda97aa8bfda6277e
sf-12 <s.fukasawa12@gmail.com>
2022-03-05 13:15:11 +0900

    vuetify導入とかrailsからデータ取得とか

---
+++ src/pages/Home.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1>Home Page</h1>
+    <v-data-table
+      :headers="headers"
+      :items="samples"
+      :items-per-page="5"
+    >
+    </v-data-table>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      headers: [
+        {text: 'ID', value: 'id', align: 'center'},
+        {text: '名前', value: 'name', align: 'center'},
+        {text: '状態', value: 'status', align: 'center'},
+      ],
+      samples: [],
+      sample: {
+        id: "",
+        name: "",
+        status: ""
+      }
+    };
+  },
+  mounted() {
+    axios.get('http://localhost:3000/api/v1/samples').then(res => {
+      console.log("res.data:",res.data['samples'])
+      this.samples = res.data['samples']
+    })
+  }
+}
+</script>
